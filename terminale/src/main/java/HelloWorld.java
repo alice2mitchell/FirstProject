7166ab3af19fe2071710c378e2370ea4404073cb
alice2mitchell <linlit93@gmail.com>
2016-11-09 19:01:21 +0300

    commit structure and pom war props. Project creates war

--- terminale/src/main/java/HelloWorld.java
+++ terminale/src/main/java/HelloWorld.java
@@ -6,32 +6,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class HelloWorld implements Servlet {
+    private ServletConfig config;
 
-private ServletConfig config;
+    public void init(ServletConfig config) throws ServletException {
+        this.config = config;
+    }
 
-public void init(ServletConfig config)throws ServletException
-        {
-        this.config=config;
-        }
+    public void destroy() {
+    }
 
-
-        public void destroy(){}
-public ServletConfig getServletConfig()
-        {
+    public ServletConfig getServletConfig() {
         return config;
-        }
-public String getServletInfo()
-        {
-        return"A Simple Servlet";
-        }
+    }
+
+    public String getServletInfo() {
+        return "A Simple Servlet";
+    }
 
 
-public void service(ServletRequest request, ServletResponse response)
-        throws ServletException,IOException
-        {
+    public void service(ServletRequest request, ServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter out=response.getWriter();
+        PrintWriter out = response.getWriter();
         out.println("<html><head>");
         out.println("<title>A Sample Servlet!</title>");
         out.println("</head>");
@@ -39,5 +37,7 @@ public void service(ServletRequest request, ServletResponse response)
         out.println("<h1>Hello, World!</h1>");
         out.println("</body></html>");
         out.close();
-        }
+    }
+
+
 }
